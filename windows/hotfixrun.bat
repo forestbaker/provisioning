213ea1872b06a234686e2cf1810a6d26187e7673
input - process - output <forestbaker@users.noreply.github.com>
2015-12-05 20:52:03 -0800

    Create hotfixrun.bat

---
+++ windows/hotfixrun.bat
@@ -0,0 +1,35 @@
+qchain.exe 
+RD /S/Q c:\hotfixes
+start /wait c:\hotfixes\win\1\Q311967_WXP_SP1_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q313450_WXP_SP1_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q314147_WXP_SP1_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q315000_WXP_SP1_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q323255_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q328310_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q328940_WXP_SP1_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q329048_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q329170_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q329390_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q329834_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q331953_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q810565_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q810577_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q810833_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q811493_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q811630_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q814033_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q815021_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q815485_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\Q817606_WXP_SP2_x86_ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB821253-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB821557-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB822603-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB823559-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB823980-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB824105-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\1\WindowsXP-KB824146-x86-ENU.exe -u -q -z 
+start /wait c:\hotfixes\win\2\js56nen.exe /q:a /r:n 
+start /wait c:\hotfixes\IE\q330994.exe /q:a /r:n
+start /wait c:\hotfixes\IE\q822925.exe /q:a /r:n
+qchain.exe 
+RD /S/Q c:\hotfixes
